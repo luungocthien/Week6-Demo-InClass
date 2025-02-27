5c2cb8a8c2dfef187d26c93ea5c25d944ad39fbb
luungocthien <ngocthien0105@gmail.com>
2025-02-27 13:33:34 +0200

    Inclass demo first commit

---
+++ src/main/java/TimeCal.java
@@ -0,0 +1,11 @@
+public class TimeCal {
+    public static void main(String[] args) {
+        System.out.println("Time needs " + timeCalculator(100.0, 100.0));
+
+
+    }
+
+    public static double timeCalculator(double x, double v) {
+        return x/v;
+    }
+}
